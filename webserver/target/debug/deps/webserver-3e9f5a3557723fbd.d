8c16953912b45c2635ccf773f2976aca47b5f503
Jan <schroeder.jan@protonmail.com>
2022-12-17 19:48:56 +0100

    initial commit

---
+++ webserver/target/debug/deps/webserver-3e9f5a3557723fbd.d
@@ -0,0 +1,5 @@
+/home/jan/Documents/dev/rust_webserver/webserver/target/debug/deps/webserver-3e9f5a3557723fbd: src/main.rs
+
+/home/jan/Documents/dev/rust_webserver/webserver/target/debug/deps/webserver-3e9f5a3557723fbd.d: src/main.rs
+
+src/main.rs:
---
+++ webserver/target/debug/webserver.d
@@ -0,0 +1,1 @@
+/home/jan/Documents/dev/rust_webserver/webserver/target/debug/webserver: /home/jan/Documents/dev/rust_webserver/webserver/src/main.rs
